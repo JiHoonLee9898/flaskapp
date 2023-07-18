fe2b461d5bb634f557d59f3711397f09bae250a2
JiHoonLee9898 <139090189+JiHoonLee9898@users.noreply.github.com>
2023-07-18 16:28:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,6 @@ hostip = socket.gethostbyname(socket.gethostname())
 req = requests.get("http://ipconfig.kr")
 out_ip = re.search(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', req.text)[1]
 
-@app.route("/")
-def home():
-    return  '컴퓨터 이름 : {0}<br>내부 IP : {1}<br>외부 IP : {2}<br><br> 당신의 IP :{3}<br><br>Hello'.format(hostname, hostip, out_ip, request.remote_addr)
+@app.route('/')
+def hello_world():
+    return 'Hello, Flask!'
