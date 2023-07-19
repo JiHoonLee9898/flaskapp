6111800a077a8dc7cdba10b931041bc2a221b3f6
JiHoonLee9898 <139090189+JiHoonLee9898@users.noreply.github.com>
2023-07-19 09:58:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ import socket
 
 app = Flask(__name__)
 
-# # 컴퓨터 이름, ip
-# hostname = socket.gethostname()
-# hostip = socket.gethostbyname(socket.gethostname())
+# 컴퓨터 이름, ip
+hostname = socket.gethostname()
+hostip = socket.gethostbyname(socket.gethostname())
 
-# #외부 IP, 아래 사이트 호출 후 결과에서 추출
-# req = requests.get("http://ipconfig.kr")
-# out_ip = re.search(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', req.text)[1]
+#외부 IP, 아래 사이트 호출 후 결과에서 추출
+req = requests.get("http://ipconfig.kr")
+out_ip = re.search(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', req.text)[1]
 
 @app.route('/')
 def hello_world():
